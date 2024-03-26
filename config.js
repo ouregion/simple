2ddf2b34211bedd51d57c17e18d1432b406ebaff
s3luan <ouregion@gmail.com>
2024-03-26 14:17:11 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+function openBot() {
+  document.getElementById("myBot").style.display = "block";
+}
+
+function closeBot() {
+  document.getElementById("myBot").style.display = "none";
+}
+
+const agentId = "clpadyra2000ejw08zt32t9xi";
